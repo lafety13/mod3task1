afb8beedccc5859776241ca466a92fd8758247dc
vadim <koozyy@bigmir.net>
2016-11-11 20:32:05 +0200

    added task 1

---
+++ task1/Bird.java
@@ -0,0 +1,7 @@
+package task1;
+
+public class Bird {
+	public void sing(String str) {
+		System.out.println("I am " + str);
+	}
+}
---
+++ task1/Main.java
@@ -0,0 +1,12 @@
+package task1;
+
+public class Main {
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		
+		bird.sing("walking");
+		bird.sing("flying");
+		bird.sing("singing");
+		bird.sing("Bird");
+	}
+}
